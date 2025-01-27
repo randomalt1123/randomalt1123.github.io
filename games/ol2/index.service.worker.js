ad4e266b8b5d1890685bc00d3d14f776a0ddd236
Eli O'Connor <94636503+Eli-code1@users.noreply.github.com>
2025-01-27 16:41:44 +0000

    Game updates

--- games/ol2/index.service.worker.js
+++ games/ol2/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1737945493|25882803420';
+const CACHE_VERSION = '1737995765|2892958737';
 /** @type {string} */
 const CACHE_PREFIX = 'Onion's Life 2-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
